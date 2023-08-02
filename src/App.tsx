82d4560ad7b795283c2480049d41089602732d0d
Kyle Dominic Mendoza <kylemendoza67@gmail.com>
2023-08-02 20:03:07 +0800

    nav bar done

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,40 @@ import "./App.css";
 
 import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Logo from "./assets/logo.png";
 
 function App() {
+  const navData = [
+    { path: "#home", name: "Home" },
+    { path: "#about", name: "About" },
+    { path: "#skills", name: "Skills" },
+    { path: "#projects", name: "Projects" },
+    { path: "#contact", name: "Contact" },
+  ];
+
   return (
     <div>
       <header>
-        <Navbar expand="lg" className="bg-body">
-          <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar expand="lg" bg="light" variant="light" className="shadow">
+          <Container className="w-75">
+            <Navbar.Brand href="#home" className="kanit">
+              <img
+                alt="Logo"
+                src={Logo}
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />{" "}
+              Kyle
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
-                    Something
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
-                </NavDropdown>
+              <Nav className="ms-auto">
+                {navData.map((item) => (
+                  <Nav.Link href={item.path} className="px-4 poppins-nav">
+                    {item.name}
+                  </Nav.Link>
+                ))}
               </Nav>
             </Navbar.Collapse>
           </Container>
