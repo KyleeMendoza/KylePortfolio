8297fcbf1e2718cf8ce577620fb4b978dc12f678
Kyle Dominic Mendoza <kylemendoza67@gmail.com>
2023-08-03 23:16:40 +0800

    tailwind update

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-
 import NavBar from "./NavBar";
+import Home from "./Home";
 
 function App() {
   return (
     <div>
       <header>
         <NavBar />
+        {/* <Container className="border border-danger">
+          <Home />
+        </Container> */}
+        {/* <Home /> */}
       </header>
     </div>
   );
--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import Logo from "./assets/logo.png";
 
 function NavBar() {
+  const [isNavOpen, setIsNavOpen] = useState(false); // initiate isNavOpen state with false
   const navData = [
     { path: "#home", name: "Home" },
     { path: "#about", name: "About" },
@@ -11,34 +11,59 @@ function NavBar() {
     { path: "#projects", name: "Projects" },
     { path: "#contact", name: "Contact" },
   ];
-  return (
-    <Navbar expand="lg" bg="light" variant="light" className="shadow">
-      <Container className="border border-info">
-        <Navbar.Brand
-          href="#home"
-          className="d-flex align-items-center kanit fs-1"
+
+  const hamburgerClose = () => {
+    return (
+      <>
+        <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+        <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+        <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+      </>
+    );
+  };
+
+  const hamburgerOpen = () => {
+    return (
+      <>
+        <svg
+          className="h-8 w-8 text-gray-600"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         >
-          <img
-            alt="Logo"
-            src={Logo}
-            width="65"
-            height="65"
-            className="d-inline-block align-top"
-          />
-          Kyle
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            {navData.map((item) => (
-              <Nav.Link href={item.path} className="px-3 bo btn-hover">
-                {item.name}
-              </Nav.Link>
-            ))}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          <line x1="18" y1="6" x2="6" y2="18" />
+          <line x1="6" y1="6" x2="18" y2="18" />
+        </svg>
+      </>
+    );
+  };
+  return (
+    <>
+      <div className="border border-1 border-red-600 px-2 flex justify-around">
+        <div className="flex items-center text-3xl">
+          <img src={Logo} alt="logo" className="w-16 h-16" />
+          <p>Kyle</p>
+        </div>
+        <div className="flex">
+          {navData.map((item) => (
+            <a href={item.path} className="px-3 flex items-center btn-hover">
+              {item.name}
+            </a>
+          ))}
+        </div>
+        <div className="border border-1 border-red-600 flex items-center">
+          <div
+            className="space-y-2"
+            onClick={() => setIsNavOpen((prev) => !prev)}
+          >
+            {isNavOpen ? hamburgerOpen() : hamburgerClose()}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
