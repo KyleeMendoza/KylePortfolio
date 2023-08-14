52a184ea8224531ee736bdd2bfd577563d3b3e6b
KyleeMendoza <kylemendoza67@gmail.com>
2023-08-15 01:23:57 +0800

    animation added and layout fixed

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button, IconButton } from "@material-tailwind/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { motion, useAnimation, useAnimationControls } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import {
   faGoogle,
   faTwitter,
@@ -31,81 +33,188 @@ function Home() {
     typeSpeed: 120,
     deleteSpeed: 80,
   });
+
+  const { ref, inView } = useInView({
+    threshold: 0.4,
+  });
+  const h1 = useAnimation();
+  const h2 = useAnimation();
+  const link = useAnimation();
+  const heroBg = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      h1.start({
+        x: 0,
+        opacity: 1,
+        transition: { type: "tween", bounce: 0.2, duration: 0.3 },
+      });
+      h2.start({
+        x: 0,
+        opacity: 1,
+        transition: { type: "tween", bounce: 0.2, duration: 0.5 },
+      });
+      heroBg.start({
+        x: 0,
+        opacity: 1,
+        transition: { type: "tween", bounce: 0.2, duration: 0.3 },
+      });
+      link.start((i) => ({
+        y: 0,
+        opacity: 1,
+        transition: {
+          type: "spring",
+          delay: i * 0.05,
+          bounce: 0.7,
+          duration: 1.5,
+        },
+      }));
+    }
+
+    if (!inView) {
+      h1.start({
+        x: -100,
+        opacity: 0,
+      });
+      h2.start({
+        x: -100,
+        opacity: 0,
+      });
+      heroBg.start({
+        x: 200,
+        opacity: 0,
+      });
+      link.start({
+        y: -30,
+        opacity: 0,
+      });
+    }
+  }, [inView]);
+
+  // useEffect(() => {
+  //   link.start((i) => ({
+  //     opacity: 1,
+  //     y: 10,
+  //     transition: {
+  //       type: "spring",
+  //       delay: i * 0.3,
+  //       bounce: 0.7,
+  //       duration: 1,
+  //     },
+  //   }));
+  // }, []);
+
   return (
     <div className="flex w-full justify-center items-center">
-      <div className="flex w-[75%]">
-        <div className="flex-1 font-['Poppins'] font-bold text-darkBlue ">
-          <p className="text-4xl">Hi There,</p>
-          <p className="text-4xl">
-            I'm a <span className="text-brightRed">{text}</span>{" "}
-            <span>
-              <Cursor cursorStyle="<" />
-            </span>{" "}
-          </p>
-          <p className="text-md font-semibold mt-4">
-            I'm <span className="text-brightRed">Kyle Mendoza</span>, an
-            aspiring Software Developer based in Pasig, Philippines
-          </p>
-          {/* <button className="border border-2 bg-darkBlue text-white px-4  rounded-full">
-            Save Changes
-          </button> */}
-          <Button className="bg-brightRed my-4">Download CV</Button>
+      <div ref={ref} className="flex w-[75%] ">
+        <div className="w-[53%] font-['Poppins'] font-bold text-darkBlue ">
+          <motion.div animate={h1}>
+            <p className="text-4xl">Hi There,</p>
+            <p className="text-4xl">
+              I'm a <span className="text-brightRed">{text}</span>{" "}
+              <span>
+                <Cursor cursorStyle="<" />
+              </span>{" "}
+            </p>
+          </motion.div>
+          <motion.div animate={h2}>
+            <p className="text-md font-semibold mt-4">
+              I'm <span className="text-brightRed">Kyle Mendoza</span>, an
+              aspiring Software Developer based in Pasig, Philippines
+            </p>
+            <Button className="bg-brightRed my-4 drop-shadow-lg">
+              Download CV
+            </Button>
+          </motion.div>
           <div className="flex gap-3">
-            <a href="https://www.facebook.com/shaclonely">
+            <motion.a
+              custom={0}
+              animate={link}
+              href="https://www.facebook.com/shaclonely"
+              className="drop-shadow-lg"
+            >
               <IconButton
                 className="rounded-full block hover:bg-[#1877F2] hover:scale-125 "
                 size="lg"
               >
                 <FontAwesomeIcon icon={faFacebook} className="flex text-lg" />
               </IconButton>
-            </a>
-            <a href="https://twitter.com/kdmendoza00">
+            </motion.a>
+            <motion.a
+              custom={1}
+              animate={link}
+              href="https://twitter.com/kdmendoza00"
+              className="drop-shadow-lg"
+            >
               <IconButton
                 className="rounded-full block hover:bg-[#1DA1F2] hover:scale-125"
                 size="lg"
               >
                 <FontAwesomeIcon icon={faTwitter} className="flex text-lg" />
               </IconButton>
-            </a>
-            <a href="https://www.instagram.com/kyleemndz/">
+            </motion.a>
+            <motion.a
+              custom={2}
+              animate={link}
+              href="https://www.instagram.com/kyleemndz/"
+              className="drop-shadow-lg"
+            >
               <IconButton
                 className="rounded-full block hover:bg-gradient-to-r from-[#F56040] via-[#E1306C] to-[#833AB4] hover:scale-125"
                 size="lg"
               >
                 <FontAwesomeIcon icon={faInstagram} className="flex text-lg" />
               </IconButton>
-            </a>
-            <a href="https://www.linkedin.com/in/kyleemendoza">
+            </motion.a>
+            <motion.a
+              custom={3}
+              animate={link}
+              href="https://www.linkedin.com/in/kyleemendoza"
+              className="drop-shadow-lg"
+            >
               <IconButton
                 className="rounded-full block hover:bg-[#0077b5] hover:scale-125"
                 size="lg"
               >
                 <FontAwesomeIcon icon={faLinkedin} className="flex text-lg" />
               </IconButton>
-            </a>
-            <a href="https://github.com/KyleeMendoza">
+            </motion.a>
+            <motion.a
+              custom={4}
+              animate={link}
+              href="https://github.com/KyleeMendoza"
+              className="drop-shadow-lg"
+            >
               <IconButton
                 className="rounded-full block hover:scale-125"
                 size="lg"
               >
                 <FontAwesomeIcon icon={faGithub} className="flex text-lg" />
               </IconButton>
-            </a>
-            <a href="https://medium.com/@kylemendoza67">
+            </motion.a>
+            <motion.a
+              custom={5}
+              animate={link}
+              href="https://medium.com/@kylemendoza67"
+              className="drop-shadow-lg"
+            >
               <IconButton
                 className="rounded-full block hover:scale-125"
                 size="lg"
               >
                 <FontAwesomeIcon icon={faMedium} className="flex text-lg" />
               </IconButton>
-            </a>
+            </motion.a>
           </div>
         </div>
-        <div className="flex-1 relative flex items-center justify-center ">
+        <motion.div
+          animate={heroBg}
+          className="flex-1 relative flex items-center justify-center"
+        >
           <div className="justify-center flex absolute ">
             <img src={formalPic} alt="Picture" className="hero-bg right-0" />
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
--- src/screens/NavBar.tsx
+++ src/screens/NavBar.tsx
@@ -15,31 +15,32 @@ function NavBar() {
 
   return (
     <>
-      <div className="md:flex justify-around items-center md:bg-white drop-shadow-lg">
-        <div className="flex justify-between items-center px-2">
-          <div className="flex items-center text-3xl">
-            <img src={Logo} alt="logo" className="w-14 h-14" />
-            <p className="font-['Poppins'] font-bold text-darkBlue">Kyle</p>
+      <div className="md:flex justify-center items-center md:bg-white drop-shadow-lg">
+        <div className="flex w-[75%] justify-between ">
+          <div className="flex justify-between items-center">
+            <div className="flex items-center text-3xl">
+              <img src={Logo} alt="logo" className="w-14 h-14" />
+              <p className="font-['Poppins'] font-bold text-darkBlue">Kyle</p>
+            </div>
+            <div className="block md:hidden">
+              <Menu isOpen={isOpen} setIsOpen={setIsOpen} />
+            </div>
           </div>
-          <div className="block md:hidden">
-            <Menu isOpen={isOpen} setIsOpen={setIsOpen} />
-          </div>
-        </div>
 
-        <div
-          className={`flex flex-col py-10 items-center gap-10 bg-darkBlue text-white ${
-            isOpen ? "opacity-100" : "opacity-0"
-          } md:opacity-100 md:flex-row md:py-0 md:bg-white md:text-black transition-all duration-400`}
-        >
-          {/* <div className="absolute w-full left-0 py-10 gap-10 flex flex-col items-center bg-darkBlue text-white opacity-100 md:opacity-100 md:flex-row md:bg-white md:text-black md:w-auto md:py-0 md:static"> */}
-          {navData.map((item) => (
-            <a
-              href={item.path}
-              className="btn-hover font-['Poppins'] font-bold text-white hover:text-brightRed transition-all ease-in-out md:text-darkBlue"
-            >
-              {item.name}
-            </a>
-          ))}
+          <div
+            className={`flex flex-col py-10 items-center gap-10 bg-darkBlue text-white ${
+              isOpen ? "opacity-100" : "opacity-0"
+            } md:opacity-100 md:flex-row md:py-0 md:bg-white md:text-black transition-all duration-400`}
+          >
+            {navData.map((item) => (
+              <a
+                href={item.path}
+                className="btn-hover font-['Poppins'] font-bold text-white hover:text-brightRed transition-all ease-in-out md:text-darkBlue"
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
         </div>
       </div>
     </>
